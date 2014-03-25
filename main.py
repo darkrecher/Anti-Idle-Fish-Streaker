d868186bbdab747044bbdf2e7a04795eb151520d
darkrecher <recher_burp@yahoo.fr>
2014-03-25 13:44:32 +0100

    screen_shot : file save + récupération pixel rgb.

--- main.py
+++ main.py
@@ -8,5 +8,7 @@ import screen_shotter
 debug("coucou éé")
 info("pouet")
 
-screen_shot = screen_shotter.ScreenShot((50, 50), (100, 100))
+screen_shot = screen_shotter.ScreenShot((50, 50), (200, 200))
 debug("shot taken")
+debug("color : " + str(screen_shot.get_pixel_rgb(49, 49)))
+screen_shot.save("J:\\Recher\\projets\\git\\Anti-Idle-Fish-Streaker\\screenshots\\paf.png")
--- screen_shotter.py
+++ screen_shotter.py
@@ -8,8 +8,8 @@ import wx
 # initialisation du  contexte
 app = wx.App(False)
 screen = wx.ScreenDC()
-object_size_screen = screen.GetSize()
-size_screen = (object_size_screen[0], object_size_screen[1])
+wxobj_size_screen = screen.GetSize()
+size_screen = (wxobj_size_screen[0], wxobj_size_screen[1])
 debug("La taille de l'écran est : %s" % str(size_screen))
 
 class ScreenShot(object):
@@ -17,9 +17,18 @@ class ScreenShot(object):
     def __init__(self, coord_upleft, size_screenshot):
         self.coord_upleft = coord_upleft
         self.size_screenshot = size_screenshot
-        bmp = wx.EmptyBitmap(size_screenshot[0], size_screenshot[1])
-        self.mem = wx.MemoryDC(bmp)
-        self.mem.Blit(0, 0, size_screenshot[0], size_screenshot[1], screen, coord_upleft[0], coord_upleft[1])
-        # TODO : ne pas sauvegarder ici, mais faire une fonction que le fait quand on a envie.
-        bmp.SaveFile("J:\\Recher\\projets\\git\\Anti-Idle-Fish-Streaker\\screenshots\\paf.png", wx.BITMAP_TYPE_PNG)
+        self.bmp = wx.EmptyBitmap(size_screenshot[0], size_screenshot[1])
+        self.mem = wx.MemoryDC(self.bmp)
+        self.mem.Blit(
+            0, 0,
+            size_screenshot[0], size_screenshot[1],
+            screen,
+            coord_upleft[0], coord_upleft[1])
+        # FUTURE : je garde comme variable membre self.bmp ET self.mem.
+        #          Est-ce bien nécessaire ?
 
+    def save(self, path_file_save):
+        self.bmp.SaveFile(path_file_save, wx.BITMAP_TYPE_PNG)
+
+    def get_pixel_rgb(self, x_pix, y_pix):
+        return self.mem.GetPixel(x_pix, y_pix)[0:3]
