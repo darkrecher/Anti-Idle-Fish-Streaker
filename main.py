ca559cdf694ce084dfe92e6c75f7c51dd1100f28
darkrecher <recher_burp@yahoo.fr>
2014-03-26 22:05:14 +0100

    screen_shotter : test unitaire, param par défaut.

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import screen_shotter
 debug("coucou éé")
 info("pouet")
 
-screen_shot = screen_shotter.ScreenShot((50, 50), (200, 200))
+screen_shot = screen_shotter.ScreenShot()
 debug("shot taken")
 debug("color : " + str(screen_shot.get_pixel_rgb(49, 49)))
 debug("color : " + str(screen_shot.get_pixel_hsv(49, 49)))
-screen_shot.save("J:\\Recher\\projets\\git\\Anti-Idle-Fish-Streaker\\screenshots\\paf.png")
+
--- screen_shotter.py
+++ screen_shotter.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from my_logging import debug, info
 
 import wx
-import colrtool
+from colrtool import hsv_from_rgb
 
 # initialisation du  contexte
 app = wx.App(False)
@@ -13,9 +13,12 @@ wxobj_size_screen = screen.GetSize()
 size_screen = (wxobj_size_screen[0], wxobj_size_screen[1])
 debug("La taille de l'écran est : %s" % str(size_screen))
 
+
 class ScreenShot(object):
 
-    def __init__(self, coord_upleft, size_screenshot):
+    def __init__(self, coord_upleft=(0, 0), size_screenshot=size_screen):
+        # TODO : si coord_upleft + size_screenshot dépasse le coin inférieur
+        # droit, ça plante. Donc il faudra créer un size_screenshot_corrected.
         self.coord_upleft = coord_upleft
         self.size_screenshot = size_screenshot
         self.bmp = wx.EmptyBitmap(size_screenshot[0], size_screenshot[1])
@@ -38,4 +41,16 @@ class ScreenShot(object):
 
     def get_pixel_hsv(self, x_pix, y_pix):
         red, grn, blu = self.mem.GetPixel(x_pix, y_pix)[0:3]
-        return colrtool.hsv_from_rgb(red, grn, blu)
+        return hsv_from_rgb(red, grn, blu)
+
+
+# Exemple d'utilisation / tests unitaires à l'arrache.
+if __name__ == '__main__':
+    DIR_SHOTS = ("J:\\Recher\\projets\\git\\"
+                 "Anti-Idle-Fish-Streaker\\screenshots\\")
+    screen_shot_mini = ScreenShot((50, 50), (200, 200))
+    screen_shot_mini.save(DIR_SHOTS + "mini.png")
+    screen_shot_big = ScreenShot()
+    debug("color : " + str(screen_shot_big.get_pixel_rgb(49, 49)))
+    debug("color : " + str(screen_shot_big.get_pixel_hsv(49, 49)))
+    screen_shot_big.save(DIR_SHOTS + "big.png")
