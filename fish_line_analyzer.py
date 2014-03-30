5046d23a5d68dbdfa3445dc1a6b49721185a35f7
darkrecher <recher_burp@yahoo.fr>
2014-03-30 09:39:35 +0200

    détection OK.
    On arrive à détecter à quel moment le triangle passe juste sur le trait
    rouge. Y'a plus qu'à envoyer la touche. En espérant que y'ait pas trop
    de temps de latence entre la détection et l'envoi de signal.

--- fish_line_analyzer.py
+++ fish_line_analyzer.py
@@ -2,9 +2,38 @@
 
 from __future__ import unicode_literals
 from my_logging import debug, info
+from enum import enum
 
 import screen_shotter
 import colrtool
+
+FISHING_STATE = enum(
+
+    "FISHING_STATE",
+
+    # La couleur cyan n'est pas dominante.
+    # On vient de capturer ou de rater un triangle, la zone de jeu
+    # est donc en train de s'illuminer en vert ou en rouge.
+    "HIGHLIGHTED",
+
+    # Le triangle est passé sur la marque rouge, on l'a détecté,
+    # et on a envoyé le signal.
+    # Il faut attendre que la marque rouge se réactualise
+    # (elle doit se déplacer à un autre endroit)
+    "SIGNAL_SENT",
+
+    # La marque rouge s'est réactualisée. On attend le triangle.
+    # Soit il est pas là du tout, soit il est là, mais encore loin.
+    "WAIT_FISH",
+
+    # Le triangle est très proche. Il faut faire des screen shot très fréquent,
+    # Afin de détecter le bon moment où le triangle passe sur la marque rouge.
+    "FISH_NEAR",
+
+    # Le triangle est pil poil sur la marque rouge. Il faut envoyer le signal.
+    "SEND_SIGNAL",
+)
+fst = FISHING_STATE
 
 class FishLineAnalyzer(object):
 
@@ -17,14 +46,16 @@ class FishLineAnalyzer(object):
         self.x_red_mark_1 = None
         self.x_red_mark_2 = None
         self.x_triangle = None
-        # state_stable = True : la couleur cyan est dominante.
-        # On attend un triangle, ou il y en a un en train de passer.
-        # state_stable = False : la couleur cyan n'est pas dominante.
-        # On vient de capturer ou de rater un triangle, la zone de jeu
-        # est donc en train de s'illuminer en vert ou en rouge.
-        self.state_stable = False
+        self.cyan_dominant = False
 
-    def analyze(self):
+        # Valeur initiale arbitraire à la con.
+        # De plus, le nommage est à l'arrache.
+        self.x_red_mark_defined = self.line_length + 50
+        self.fst_cur = fst.HIGHLIGHTED
+        self.x_red_mark_1_prev = None # TODO : useless ??
+
+    def analyze_screenshot(self):
+        # TODO : séparer ça dans des fonctions plus petites.
         screenshot = screen_shotter.ScreenShot(
             (self.x1_line, self.y_line),
             (self.line_length, 1))
@@ -33,7 +64,7 @@ class FishLineAnalyzer(object):
         self.x_red_mark_2 = None
         self.x_triangle = None
         nb_cyan_pix = 0
-        hue_diff_max = 0
+        hue_diff_min = 361
 
         for x_line in range(self.line_length):
             rgb_pix = screenshot.get_pixel_rgb((x_line, 0))
@@ -52,11 +83,11 @@ class FishLineAnalyzer(object):
 
             else:
                 hsv_pix = colrtool.hsv_from_rgb(*rgb_pix)
-                # hue=192 correspond au cyan (0, 204, 255)
+                # hue=35 correspond à l'orange = la pointe du triangle.
                 # TODO : foutre tous ces trucs dans des constantes.
-                hue_diff = abs(hsv_pix[0] - 192)
-                if hue_diff > 50 and hue_diff_max < hue_diff:
-                    hue_diff_max = hue_diff
+                hue_diff = abs(hsv_pix[0] - 35)
+                if hue_diff < 50 and hue_diff_min > hue_diff:
+                    hue_diff_min = hue_diff
                     self.x_triangle = x_line
 
         if (self.x_red_mark_1 is not None
@@ -67,12 +98,80 @@ class FishLineAnalyzer(object):
             raise Exception("Les deux marques rouge ne sont pas adjacentes")
 
         if nb_cyan_pix < self.line_length / 2:
-            self.state_stable = False
+            self.cyan_dominant = False
         else:
-            self.state_stable = True
+            self.cyan_dominant = True
 
-        debug(" ".join((
-            "state_stable:", str(self.state_stable),
+    def log_current_info(self):
+        info(" ".join((
+            "cyan_dom:", str(self.cyan_dominant),
             "red_mark:", str(self.x_red_mark_1),
+            "x_red_mark_defined:", str(self.x_red_mark_defined),
             "x_triangle", str(self.x_triangle)
         )))
+        self.log_diff()
+
+    def log_diff(self):
+        if self.x_triangle is not None and self.x_red_mark_1 is not None:
+            debug("diff " + str(abs(self.x_triangle - self.x_red_mark_1)))
+
+
+    # TODO : Fonction interne. Et une fonction externe qui enchaine tout.
+    def refresh_current_state(self):
+
+        if not self.cyan_dominant:
+            self.fst_cur = fst.HIGHLIGHTED
+            self.x_red_mark_defined = None
+            return
+
+        if self.x_red_mark_1 is not None:
+            self.x_red_mark_defined = self.x_red_mark_1
+
+        # TODO : crap
+        #if (self.x_red_mark_1_prev is None
+        #    or abs(self.x_red_mark_1_prev-self.x_red_mark_1 > 1)):
+        #
+        #    # la marque vient de changer. On réactualise. À priori,
+        #    # le triangle n'est pas encore là. Il arrivera plus tard.
+        #    self.x_red_mark_1_prev = self.x_red_mark_1
+        #    self.fst_cur = fst.WAIT_FISH
+        #    return
+
+        if self.x_triangle is None:
+            # Le triangle n'est pas encore là.
+            self.fst_cur = fst.WAIT_FISH
+            return
+
+        if self.x_red_mark_defined is not None:
+
+            if (self.x_red_mark_defined == self.x_triangle
+                or self.x_red_mark_defined == self.x_triangle + 1):
+
+                # On est dessus. Envoyez le signal ! (Sauf si déjà fait)
+                if self.fst_cur == fst.SEND_SIGNAL:
+                    self.fst_cur = fst.SIGNAL_SENT
+                else:
+                    self.fst_cur = fst.SEND_SIGNAL
+                return
+
+            else:
+
+                # TODO : valeur de seuil complètement à l'arrache.
+                if abs(self.x_triangle - self.x_red_mark_defined) < 10:
+                    # Le triangle est proche. Faut rafraîchir souvent.
+                    self.fst_cur = fst.FISH_NEAR
+                else:
+                    # Le triangle est là, mais loin. Pas de quoi s'affoler.
+                    self.fst_cur = fst.WAIT_FISH
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from my_logging import debug, info
 
 import screen_shotter
 import fish_line_detector
-from fish_line_analyzer import FishLineAnalyzer
+from fish_line_analyzer import FishLineAnalyzer, fst
 
 debug("coucou éé")
 info("pouet")
@@ -17,7 +17,27 @@ if fish_line_info is None:
     raise SystemExit(1)
 
 fish_line_analyzer = FishLineAnalyzer(*fish_line_info)
+fst_prev = fst.HIGHLIGHTED
 
 while True:
-    fish_line_analyzer.analyze()
-    time.sleep(0.5)
+
+    fish_line_analyzer.analyze_screenshot()
+    fish_line_analyzer.refresh_current_state()
+    fst_cur = fish_line_analyzer.fst_cur
+
+    if fst_prev != fst_cur:
+        info(" ".join((
+            str(fst.dictReverse[fst_prev]),
+            "->",
+            str(fst.dictReverse[fst_cur])
+        )))
+        fish_line_analyzer.log_current_info()
+        info("")
+        fst_prev = fst_cur
+
+    # TODO : une valeur spécifique pour chaque état.
+    if fst_cur == fst.FISH_NEAR:
+        time.sleep(0.00001)
+        #fish_line_analyzer.log_current_info()
+    else:
+        time.sleep(0.25)
